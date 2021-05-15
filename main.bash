2919ac6c61b2bcb9d0768e9f650da3821a47cd7b
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-15 16:12:57 -0400

    Update main.bash

--- main.bash
+++ main.bash
@@ -1,1 +1,7 @@
-echo "hi";
+echo "Node.JS Web Server Template is starting..."
+echo "Starting server/main.sh"
+echo "█▀▄▀█ ▄▀█ █ █▄░█ ░ █▀ █░█
+█░▀░█ █▀█ █ █░▀█ ▄ ▄█ █▀█"
+bash server/main.sh
+npm install express
+node server.js
