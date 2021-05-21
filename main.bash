47b1755da4d146f30882dcbeb23b2a7f7fbecafb
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-21 10:31:00 -0400

    Update and rename main.bash to main.sh

--- main.bash
+++
@@ -1,7 +0,0 @@
-echo "Node.JS Web Server Template is starting..."
-echo "Starting server/main.sh"
-echo "█▀▄▀█ ▄▀█ █ █▄░█ ░ █▀ █░█
-█░▀░█ █▀█ █ █░▀█ ▄ ▄█ █▀█"
-bash server/main.sh
-npm install express
-node server.js
---
+++ main.sh
@@ -0,0 +1,9 @@
+lang=shell script
+
+
+echo Starting server/main.sh
+echo █▀▄▀█ ▄▀█ █ █▄░█ ░ █▀ █░█
+█░▀░█ █▀█ █ █░▀█ ▄ ▄█ █▀█
+bash server/main.sh
+npm install express
+node server.js
